06771c841b616434e309835ca828534e7021e667
devux <devakumar.mj@gmail.com>
2016-11-01 18:16:52 +0530

    Petition form

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,4 +1,4 @@
 class Petition < ApplicationRecord
-  has_many :comments, dependent: :destroy
-  has_many :petition_advisors, dependent: :destroy
+#   has_many :comments, dependent: :destroy
+#   has_many :petition_advisors, dependent: :destroy
 end
